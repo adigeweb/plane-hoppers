400f0a2a636b6560df724757082156e6a155524a
adigeweb <138293403+adigeweb@users.noreply.github.com>
2023-08-12 12:29:48 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+document.querySelector("button#basla").addEventListener("click", () => {
+    location.href = "oyun.html";
+});
+
+document.querySelector("button#dukkan").addEventListener("click", () => {
+    location.href = "dukkan.html";
+});
